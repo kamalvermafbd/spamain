f31665eed4c6ab44249cc76f1d8882f9ad4ab0b8
kamalvermafbd <167846192+kamalvermafbd@users.noreply.github.com>
2026-04-10 14:16:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import HomeLibraryPrefetch from "./HomeLibraryPrefetch";
 import ServicesPrefetch from "./ServicesPrefetch";
 import PremiumBookingModalMock from "./PremiumBookingModalMock";
 import MembershipPrefetch from "./MembershipPrefetch";
+import MembershipPlans from "./MembershipPlans";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { 
